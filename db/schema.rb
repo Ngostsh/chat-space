1cd20f646353c7edb047c822a4091067038beb9e
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-08-21 19:39:10 +0900

    mod devise-user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190821101114) do
+ActiveRecord::Schema.define(version: 20190821103416) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20190821101114) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -38,10 +37,7 @@ ActiveRecord::Schema.define(version: 20190821101114) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
 end
