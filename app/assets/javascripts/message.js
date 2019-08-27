c4f1fcac0ff8ec728f3fe96fd88777bf5640d449
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-08-27 19:06:21 +0900

    add-Automatic-update-function
    自動更新機能を実装した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="message" message-id=${message.id}>
+      `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -66,4 +66,28 @@ $(function(){
       $('.form__submit').attr('disabled', false);
     })
   })
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $(".message:last").data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    };
+  }
+  setInterval(reloadMessages, 5000);
 });
