b9294d7f9316db5148c4cfb6dabd681c9e2c9b11
Ngostsh <polypterus_delhezy_from_congo@yahoo.co.jp>
2019-08-27 15:03:06 +0900

    add-incremental-search
    インクリメンタルサーチ機能を実装
    また、リロードしないとjavascriptが作動しないエラーの原因であるturbolinksを削除

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,60 @@
+$(function() {
+
+  var search_list = $("#user-search-result");
+  var selected_list = $("#chat-group-users");
+  
+  function appendListUser(user) {
+     var html = `<div class="chat-group-user clearfix">
+     <p class="chat-group-user__name">${ user.name }</p>
+     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+   </div>`
+      search_list.append(html);
+  }
+
+  function appendUser(user_id, user_name) {
+    var html = `<div class='chat-group-user'>
+    <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+    <p class='chat-group-user__name'>${ user_name }</p>
+    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+  </div>`
+      selected_list.append(html);
+  }
+  
+    $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      
+      .done(function(users) {
+      $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendListUser(user);
+          });
+        }
+        else {
+          $("#user-search-result").empty();
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    });
+    $(document).on("click", ".user-search-add", function() {
+      var user_id = $(this).data('user-id');
+      var user_name = $(this).data('user-name');
+      appendUser(user_id, user_name);
+    })
+    $(document).on("click", ".user-search-add", function() {
+      var parent = $(this).parent();
+      $(parent).remove();
+    })
+    $(document).on("click", ".user-search-remove", function() {
+      var parent = $(this).parent();
+      $(parent).remove();
+    })
+  });
